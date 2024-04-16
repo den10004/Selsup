310a229e2a729e604a95f3f5124d33e213b74b40
Denis <denis-sergeev-85@inbox.ru>
2024-04-16 12:09:48 +0300

    сделано

---
+++ src/ParamEditor.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "./App.css";
+
+interface Params {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: Params[];
+}
+
+function ParamEditor() {
+  const [purpose, setPurpose] = useState("");
+  const [length, setLength] = useState("");
+  const [total, setTotal] = useState<Model>();
+
+  const getModel = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    const model: Model = {
+      paramValues: [
+        {
+          paramId: 1,
+          value: purpose,
+        },
+        {
+          paramId: 2,
+          value: length,
+        },
+      ],
+    };
+
+    setTotal(model);
+  };
+
+  if (total !== undefined) {
+    console.log(total);
+  }
+
+  return (
+    <>
+      <form className="form">
+        <div>
+          <label>Назначение: </label>
+
+          <input
+            type="text"
+            onChange={(e) => setPurpose(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Длина: </label>
+
+          <input
+            type="text"
+            onChange={(e) => setLength(e.target.value)}
+          ></input>
+        </div>
+        <button type="submit" onClick={getModel}>
+          Отправить
+        </button>
+      </form>
+
+      <div>
+        <span>{total?.paramValues[0].value} </span>
+        <br></br>
+        <span>{total?.paramValues[0].value} </span>
+      </div>
+    </>
+  );
+}
+
+export default ParamEditor;
